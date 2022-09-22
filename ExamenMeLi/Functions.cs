c4f390ee0d2ce79fe374439fed81bafe0d0e1ea3
Wenseslao Citera <wcitera@yacare.prod>
2022-09-22 12:43:16 -0300

    First commit

---
+++ ExamenMeLi/Functions.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExamenMeLi
+{
+    static public class Functions
+    {
+        public static bool IsMutant(string[] dna) {
+            /*
+             * A T G C G A
+               C A G T G C
+               T T A T G T
+               A G A A G G
+               C C C C T A
+               T C A C T G
+            */
+
+            bool ismutant = false;
+            int rango = dna.Length;
+            char[,] arregloChar = new char[rango, rango];
+            int contSecuencia = 0;
+            
+            for (int i = 0; i < rango; i++){
+                char[] row = dna[i].ToCharArray();
+                for (int j = 0; j < rango; j++) {
+                    arregloChar[i, j] = row[j];
+                }
+            }
+            
+            for(int i = 0; i<rango; i++)
+            {
+                for (int j = 0; j<rango; j++)
+                {
+                    char letra = arregloChar[i, j];
+                    int cont =0;
+                    int auxi = i;
+                    int auxj = j;
+                    while (auxj+1 < rango && cont < 4)
+                    {
+                        char segundaLetra = arregloChar[auxi, auxj++];
+                        if (letra == segundaLetra)
+                        {
+                            cont++;
+                        }
+                    }
+                    if (cont >= 4) contSecuencia++;
+
+                    auxi = i;
+                    auxj = j;
+                    cont = 0;
+                    while (auxi+1 < rango && cont < 4) {
+                        char segundaLetra = arregloChar[auxi++, auxj];
+                        if (letra == segundaLetra)
+                        {
+                            cont++;
+                        }
+                    }
+                    if (cont >= 4) contSecuencia++;
+
+                    
+                    auxi = i;
+                    auxj = j;
+                    cont = 0;
+                    while (auxi+1 < rango && auxj+1 < rango && cont < 4)
+                    {
+                        char segundaLetra = arregloChar[auxi++, auxj++];
+                        if (letra == segundaLetra)
+                        {
+                            cont++;
+                        }
+
+                    }
+                    if (cont >= 4) contSecuencia++;
+                }
+            }
+
+            Console.WriteLine(contSecuencia);
+            if (contSecuencia > 1) ismutant = true;
+            
+            return ismutant;
+        }
+    }
+}
---
+++ ExamenMeLi/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExamenMeLi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] dna = new string[] { "AAAAGA", "CAGTGC", "TTATGT", "AGAAGG", "CCCCTA", "TCACTG" };
+            List<string[]> strings = new List<string[]>();
+            strings.Add(new string[] { "ATGCGA", "CAGTGC", "TTATGT", "AGAAGG", "CCCCTA", "TCACTG" }); //true
+            strings.Add(new string[] { "AAAACG", "TTTTGC", "CGGCCC", "CAGACA", "TACCCC", "GGGGAG" }); 
+            strings.Add(new string[] { "GTGCGA", "CAGAGC", "TTATGT", "AGAAGG", "CACCTA", "TCACTG" });
+            foreach (string[] dna in strings) {
+                Console.WriteLine(Functions.IsMutant(dna));
+            }
+            
+            Console.ReadKey();
+        }
+
+       
+    }
+}
